e2b2dc3a9e9c86f5e0a636a161b3645b5e0b1d2e
Mandy Farrugia <87620808+deefarrugia2001@users.noreply.github.com>
2021-07-19 11:18:29 +0200

    Implemented code to design and develop the tallinja database.
    Tables have been created in such a way that the customers will contain a list of customer numbers, whereby each customer number should be unique. Moreover, the customer will have a list of balance checks, as well as the date at which they checked for their balance.

--- Malta Public Transport - Database.sql
+++ Malta Public Transport - Database.sql
@@ -8,3 +8,17 @@ BEGIN
 	USE master; -- Switch control to the default database.
 	DROP DATABASE tallinja; -- Delete the tallinja database.
 END
+
+CREATE DATABASE tallinja; -- Create the database tallinja.
+USE tallinja; -- Switch control to tallinja in order to create tables within this
+
+CREATE TABLE customers (
+	customer_id UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+	customer_number INTEGER UNIQUE NOT NULL CHECK (LEN(customer_number) = 8) -- Check that the customer number is exactly eight characters long.
+);
+
+CREATE TABLE customers_balance (
+	customer_id UNIQUEIDENTIFIER DEFAULT NEWID() FOREIGN KEY REFERENCES customers(customer_id),
+	balance DECIMAL(6,2) NOT NULL CHECK (balance < 0), -- Check that the balance is greater than 0.
+	date DATE NOT NULL
+);
