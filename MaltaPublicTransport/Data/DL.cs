061d6799f951148339deffcd4d46d802c9bf4550
Mandy Farrugia <87620808+deefarrugia2001@users.noreply.github.com>
2021-07-19 14:22:47 +0200

    Installed Entity Framework for the Data layer.

--- MaltaPublicTransport/Data/DL.cs
+++ MaltaPublicTransport/Data/DL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,8 @@ namespace Data
 {
     public class DL
     {
+        tallinjaEntities mptDB = new tallinjaEntities();
+
+
     }
 }
