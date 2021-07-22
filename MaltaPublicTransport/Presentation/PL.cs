70ebe1a329333a5a1c01254437df154f36c575f6
Mandy Farrugia <87620808+deefarrugia2001@users.noreply.github.com>
2021-07-22 19:37:32 +0200

    Imported System.Thread.

--- MaltaPublicTransport/Presentation/PL.cs
+++ MaltaPublicTransport/Presentation/PL.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Presentation
 {
@@ -13,21 +14,7 @@ namespace Presentation
         static BL businessLayer = new BL();
 
         static void Main(string[] args)
-        {
-            //businessLayer.RemoveCustomer(41187967);
-            //businessLayer.AddCustomer(41187967);
-            int count = businessLayer.GetAdmissionsOnDate(20, 7, 2021);
-            Console.WriteLine($"Number of admissions: {count}");
-
-            //businessLayer.NavigateToCheckBalance();
-            //businessLayer.CommitToCustomers(Command.DELETE, 41187967);
-
-            //string statement = businessLayer.FetchCustomerStatement(14161828);
-            //Console.WriteLine(statement);
-
-            //Guid customerId = businessLayer.FetchCustomerID(14161828);
-            //Console.WriteLine(customerId);
-
+        {    
             Console.ReadLine();
         }
     }
