2bfd3b4d78c83e92a09bd61b918ae69238160202
Mandy Farrugia <87620808+deefarrugia2001@users.noreply.github.com>
2021-07-26 00:06:37 +0200

    Moved code to IO class.

--- MaltaPublicTransport/Presentation/PL.cs
+++ MaltaPublicTransport/Presentation/PL.cs
@@ -8,54 +8,10 @@ namespace Presentation
 {
     class PL
     {
-        enum Type 
-        {
-            ERROR, SUCCESS
-        }
-
         int customerNumber;
 
         static BL businessLayer = new BL();
         static Login login = new Login();
-
-        static ConsoleColor ChangeForegound(Type type) 
-        {
-            //Set the initial value to the current foreground colour.
-            ConsoleColor foregroundColour = Console.ForegroundColor;
-
-            switch(type) 
-            {
-                case Type.ERROR:
-                    foregroundColour = ConsoleColor.Red;
-                    break;
-                case Type.SUCCESS:
-                    foregroundColour = ConsoleColor.Green;
-                    break;
-            }
-
-            return foregroundColour;
-        }
-
-        static void Print(Type type, string message) 
-        {
-            Console.ForegroundColor = ChangeForegound(type);
-            Console.WriteLine(message);
-            Thread.Sleep(2000);
-            Console.ResetColor();
-        }
-
-        static void Print(string message)
-        {
-            Console.WriteLine(message);
-        }
-
-        static void Print(string message, bool skipLine) 
-        {
-            if (skipLine)
-                Console.Write($"{message}\n");
-            else
-                Console.Write(message);
-        }
 
         static void Main(string[] args)
         {
--- MaltaPublicTransport/Presentation/Presentation/Print/IO.cs
+++ MaltaPublicTransport/Presentation/Presentation/Print/IO.cs
@@ -3,10 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Presentation.Presentation.Print
 {
-    class Print
+    class IO
     {
+        enum Type
+        {
+            ERROR, SUCCESS
+        }
+
+        static ConsoleColor ChangeForegound(Type type)
+        {
+            //Set the initial value to the current foreground colour.
+            ConsoleColor foregroundColour = Console.ForegroundColor;
+
+            switch (type)
+            {
+                case Type.ERROR:
+                    foregroundColour = ConsoleColor.Red;
+                    break;
+                case Type.SUCCESS:
+                    foregroundColour = ConsoleColor.Green;
+                    break;
+            }
+
+            return foregroundColour;
+        }
+
+        static void Print(Type type, string message)
+        {
+            Console.ForegroundColor = ChangeForegound(type);
+            Console.WriteLine(message);
+            Thread.Sleep(2000);
+            Console.ResetColor();
+        }
+
+        static void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static void Print(string message, bool skipLine)
+        {
+            if (skipLine)
+                Console.Write($"{message}\n");
+            else
+                Console.Write(message);
+        }
     }
 }
