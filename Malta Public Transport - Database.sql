1f1c59aa4e33793d77809dbf56756e53969071dc
Mandy Farrugia <87620808+deefarrugia2001@users.noreply.github.com>
2021-07-26 20:09:11 +0200

    Modified CustomersBalance.

--- Malta Public Transport - Database.sql
+++ Malta Public Transport - Database.sql
@@ -20,7 +20,7 @@ BEGIN
 	);
 
 	CREATE TABLE CustomersBalance (
-		customer_id INTEGER IDENTITY(1,1) FOREIGN KEY REFERENCES Customers(customer_id) PRIMARY KEY,
+		customer_id INTEGER FOREIGN KEY REFERENCES Customers(customer_id) PRIMARY KEY,
 		balance DECIMAL(6,2) NOT NULL CHECK (balance > 0), -- Check that the balance is greater than 0.
 		date DATETIME NOT NULL
 	);
