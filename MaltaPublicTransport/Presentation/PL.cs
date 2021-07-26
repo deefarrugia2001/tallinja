eff56605702723aaf67988f3666bb600be2b5d71
Mandy Farrugia <87620808+deefarrugia2001@users.noreply.github.com>
2021-07-26 13:45:04 +0200

    Added code for Login.

--- MaltaPublicTransport/Presentation/PL.cs
+++ MaltaPublicTransport/Presentation/PL.cs
@@ -11,6 +11,7 @@ namespace Presentation
     {
         static bool isProgramRunning = true;
         static int customerNumber;
+        static bool isInputFormatCorrect = false;
 
         static BL businessLayer = new BL();
         static Login login = new Login();
@@ -23,12 +24,10 @@ namespace Presentation
 
                 IO.Print(login.AddOptionsToMenu());
                 IO.Print("Please select an option from the menu: ", false);
-                bool isByte = byte.TryParse(Console.ReadLine(), out selection);
+                isInputFormatCorrect = byte.TryParse(Console.ReadLine(), out selection);
 
-                if(isByte) 
-                {
+                if(isInputFormatCorrect) 
                     ProceedLoginMenu(selection);
-                }
             }
             while(isProgramRunning);
         }
@@ -51,6 +50,17 @@ namespace Presentation
 
         static void Login() 
         {
+            Console.Clear();
+            IO.Print("Customer Number: ", false);
+            isInputFormatCorrect = int.TryParse(Console.ReadLine(), out customerNumber);
+
+            if(isInputFormatCorrect) 
+            {
+                if (businessLayer.ValidateCustomerNumber(customerNumber))
+                    IO.Print(IO.Type.SUCCESS, "The customer number entered matches the records in our database.");
+                else
+                    IO.Print(IO.Type.ERROR, "Sorry, the customer number entered does not match the records in our database.");
+            }
         }
 
         static void WarnOutOfRange() 
